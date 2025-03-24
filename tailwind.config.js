67da2aa5f8c42b3160c2f5216ab621b03dd38a25
developer <developer@pirsani.id>
2025-03-24 16:54:13 +0100

    init laravel

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.tsx'
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+
+  plugins: [forms],
+
+  darkMode: 'class' // Just add this line at the bottom.
+};
